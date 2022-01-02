e5e6d9dde9565a95d9197ca39fd51249032ce2d4
Zeynep Şanlı <zeynep.sanli@stu.fsm.edu.tr>
2022-01-03 01:05:21 +0300

    feat: Add firebase db module

--- src/pages/BikesMap/BikesMap.js
+++ src/pages/BikesMap/BikesMap.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView, View } from 'react-native'
 import MapView from 'react-native-maps';
+import database from '@react-native-firebase/database';
 
 const BikesMap = () => {
+    async function fetchBikes(params) {
+        const bikes = await database().ref().once('value');
+        console.log(bikes.val());
+    }
+
+    useEffect(() => {
+        fetchBikes();
+    }, []);
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <MapView style={{ flex: 1 }} />
+            <MapView
+                style={{ flex: 0.5 }}
+                initialRegion={{
+                    latitude: 41.01384,
+                    longitude: 28.94966,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.05,
+                }}
+            />
         </SafeAreaView>
     );
 }
